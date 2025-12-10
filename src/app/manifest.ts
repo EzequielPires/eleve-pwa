01941499c35affd8af8b17e4ad95fee31e480795
EzequielPires <ezequiel.pires082000@gmail.com>
2025-12-10 12:17:28 -0300

    Commit

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,4 +1,4 @@
-/* import { MetadataRoute } from "next";
+import { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
@@ -23,4 +23,3 @@ export default function manifest(): MetadataRoute.Manifest {
     ]
   };
 }
- */
